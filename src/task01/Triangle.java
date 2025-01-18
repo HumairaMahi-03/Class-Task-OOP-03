81e3dfc6eeb54412b64ee57006ec046f8f873c7e
HumairaMahi-03 <humairamumtahinmahi@gmail.com>
2025-01-18 11:45:41 +0600

    task-01

---
+++ src/task01/Triangle.java
@@ -0,0 +1,13 @@
+package task01;
+
+public class Triangle {
+
+        public static void main(String[] args){
+            double b=5;
+            double h=10;
+            double area = 0.5*b*h;
+            System.out.println("the area of the triangle is: "+area);
+        }
+    }
+
+
